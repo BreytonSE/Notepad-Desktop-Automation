bde5397239680835460552eca57353f8fdc19f88
BreytonSE <breytonseanernstzen1224@gmail.com>
2026-02-18 10:20:58 +0200

    Explicit wait added to method

--- src/test/java/com/notepad/windows/NotepadWindow.java
+++ src/test/java/com/notepad/windows/NotepadWindow.java
@@ -3,17 +3,22 @@ package com.notepad.windows;
 import io.appium.java_client.windows.WindowsDriver;
 import io.appium.java_client.windows.WindowsElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static com.notepad.locators.NotePadLocators.textEditor;
 
 public class NotepadWindow {
     private final WindowsDriver<WindowsElement> driver;
+    private final WebDriverWait wait;
 
     public NotepadWindow(WindowsDriver<WindowsElement> driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
     }
 
     public void typeText(String text){
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className(textEditor)));
         driver.findElement(By.className(textEditor)).sendKeys(text);
     }
 }
