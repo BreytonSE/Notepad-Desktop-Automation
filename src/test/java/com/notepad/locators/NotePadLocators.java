c7b7ff5cc4fa04d30b6e4cce10f643c0f9c5e79f
BreytonSE <breytonseanernstzen1224@gmail.com>
2026-02-18 12:07:54 +0200

    Notepad locators class removed. Not needed. Unused imports removed from classes.

--- src/test/java/com/notepad/locators/NotePadLocators.java
+++
@@ -1,5 +0,0 @@
-package com.notepad.locators;
-
-public class NotePadLocators {
-    public static final String textEditor = "RichEditD2DPT";
-}
--- src/test/java/com/notepad/windows/NotepadWindow.java
+++ src/test/java/com/notepad/windows/NotepadWindow.java
@@ -2,19 +2,12 @@ package com.notepad.windows;
 
 import io.appium.java_client.windows.WindowsDriver;
 import io.appium.java_client.windows.WindowsElement;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static com.notepad.locators.NotePadLocators.textEditor;
 
 public class NotepadWindow {
     private final WindowsDriver<WindowsElement> driver;
-    private final WebDriverWait wait;
 
     public NotepadWindow(WindowsDriver<WindowsElement> driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver, 10);
     }
 
     public void typeText(String text){
